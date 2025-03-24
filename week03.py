61549ce86d4abdc010ab08d6a049693d0e3ea8be
batjjong <batjjong@gmail.com>
2025-03-24 14:56:44 +0900

    v0.7 set

--- week03.py
+++ week03.py
@@ -1,8 +1,4 @@
-groups =['Hot','Seventeen','Black Pink','NJZ']
-rating = [1,2,3,4]
-#zip함수는 각 두개의 리스트를 튜플로 연결
-
-group_rating = list(zip(groups,rating))
-print(group_rating)
-
+cities = ['Suwon','Hwasnug','Incheon','Bucheon','Incheon','Seoul']
+cities = set(cities)
+print(cities)
 
