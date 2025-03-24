1757cd8bca083e86df541bf1b5d344d462b95715
batjjong <batjjong@gmail.com>
2025-03-24 14:34:01 +0900

    v0.5 commit change

--- week02.py
+++ week02.py
@@ -1,7 +1,20 @@
-n = int(input("정수 입력:"))
-result = 0
-#for i in range(1, n+1):
-#    result = result + i
-result = n * (n+1)//2  # O(1)
+import random
 
-print(result)
+ran = random.randint(1, 100)
+win = False
+
+for i in range(7):
+    print(f"{7-i}번 남았습니다")
+    n = int(input("정수 입력:"))
+    if n < ran:
+        print("그 수보다 큽니다")
+    elif n > ran:
+        print("그 수보다 작습니다")
+    elif n == ran:
+        print("정답입니다")
+        win = True
+        break
+if win:
+    print("win")
+else:
+    print(f"lose answer is {ran}")
