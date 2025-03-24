6c9af2aace44e74e028ecf8f0a0a885b3714341e
batjjong <batjjong@gmail.com>
2025-03-24 15:06:35 +0900

    v0.7 set2

--- week03.py
+++ week03.py
@@ -1,4 +1,18 @@
-cities = ['Suwon','Hwasnug','Incheon','Bucheon','Incheon','Seoul']
-cities = set(cities)
-print(cities)
+def duplicate_city(cities):
+    result_city = list()
+    s= set()
 
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+    return result_city
+
+cities = ['Suwon','Hwasnug','Incheon','Bucheon','Incheon','Seoul']
+
+cities.append('Seoul')
+cities.append('Anyang')
+print(cities)
+print(set(duplicate_city(cities)))
