c65173afe2a07c0b3efcb4a4b94a69783e663a28
batjjong <batjjong@gmail.com>
2025-04-07 15:40:07 +0900

    v1.7 StackTest(My)

--- week05.py
+++ week05.py
@@ -1,11 +1,20 @@
-s1 =list()
-print(len(s1))
-s1.append("Data structure")#push
-s1.append("Database")#push
-print(len(s1))
-print(s1[-1])#top : 가장 위에 있는 값 출력
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
+def check_parentheses(a_string):
+    stack = []
+    for c in a_string:
+        if c == "(":
+            stack.append(c)
+        if c == "}" or c == "]":
+            return "ad"
+
+        if c == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+
+
+
+
+
+print(check_parentheses("("))
