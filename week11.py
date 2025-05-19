e68670e98ccce0cef592417ec2ce917c8080133c
batjjong <batjjong@gmail.com>
2025-05-19 13:55:59 +0900

    v2.9 Graph(self)

--- week11.py
+++ week11.py
@@ -6,7 +6,7 @@ class Graph:
 
 G1 = Graph(4)
 G2 = Graph(4)
-#G_self = Graph(4)
+G_self = Graph(4)
 
 # 0 == A, 1 == B, 2 == C, 3 == D
 G1.graph[0][1] = 1; G1.graph[0][2] = 1; G1.graph[0][3] = 1
@@ -32,13 +32,12 @@ for r in range(G2.SIZE):
     print()
 
 # 0 == A, 1 == B, 2 == C, 3 == D
-#G_self.graph[0][3] = 1
-#G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
-#G_self.graph[2][1] = 1
-#G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
-
-#print("G_self 무방향 그래프")
-#for r in range(G_self.SIZE):
-#    for c in range(G_self.SIZE):
-#        print(G_self.graph[r][c], end=' ')
-#    print()
+G_self.graph[0][3] = 1
+G_self.graph[1][2] = 1; G_self.graph[1][3] = 1
+G_self.graph[2][1] = 1
+G_self.graph[3][0] = 1; G_self.graph[3][1] = 1
+print("G_self 무방향 그래프")
+for r in range(G_self.SIZE):
+    for c in range(G_self.SIZE):
+        print(G_self.graph[r][c], end=' ')
+    print()
